a5750353ac8b703167c45a24db4ef9010c711450
robby <robbymanis@tutanota.com>
2022-11-07 22:03:56 -0500

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,27 @@
 require('plugins') --.config/nvim/lua/plugins.lua
+-- :help options
 vim.api.nvim_command('set tgc')
 vim.g.nobackup = true
 vim.g.nowritebackup = true
 vim.g.setupdatetime = 300
-vim.g.signcolumn = yes
+vim.g.signcolumn = 'yes'
+vim.opt.signcolumn = 'yes'
 vim.api.nvim_command('colo gruvbox8')
-
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.updatetime = 300
+vim.opt.shiftwidth = 2
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.g.mapleader = ' '
 -- vim.api.nvim_set_keymap('n', ']g', '<Plug>(coc-diagnostic-next)<CR>',{})
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>',{}) -- remaps the leader key to be space. this has to be first
 vim.api.nvim_set_keymap('n', ']g', '<Cmd>CocDiagnostics next<CR>',{})
 vim.api.nvim_set_keymap('n', '[g', '<Cmd>CocDiagnostics prev<CR>',{})
-
+vim.api.nvim_set_keymap('n', 'ff', '<Cmd>CocCommand prettier.forceFormatDocument<CR>',{})
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)<CR>',{})
+vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)<CR>',{})
+vim.api.nvim_set_keymap('n', 'rn', '<Plug>(coc-rename)<CR>',{})
+vim.api.nvim_set_keymap('n', 'K', '<Cmd>call ShowDocumentation()<CR>',{})
+vim.api.nvim_set_keymap('n', '<Leader>e', ':Lex 15<CR>',{})
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>',{})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,8 +1,18 @@
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use {'neoclide/coc.nvim', branch='release'}
+	use 'nvim-lua/plenary.nvim' --required by telescope
+	use 'nvim-tree/nvim-web-devicons'
+	use 'nvim-telescope/telescope.nvim' -- also requires ripgrep
 	use 'mattn/emmet-vim'
 	use 'prettier/vim-prettier'
 	use 'rafamadriz/friendly-snippets'
 	use 'lifepillar/vim-gruvbox8'
+	use {
+	  "startup-nvim/startup.nvim",
+	requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+	config = function()
+	  require"startup".setup()
+	end
+      }
 end)
